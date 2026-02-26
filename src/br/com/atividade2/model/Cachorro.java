57dfaf99ce58c35740876bd5a56ed4a1336a1cb1
Riquelme Santos <riquelmesmatamoh@gmail.com>
2026-02-26 15:23:31 -0300

    corrigindo métodos e validações

--- src/br/com/atividade2/model/Cachorro.java
+++ src/br/com/atividade2/model/Cachorro.java
@@ -15,26 +15,42 @@ public class Cachorro {
     	this.peso = peso;
     }
     
-    public void exibirFaixaEtaria (int idade_animal) {
-    	if (idade_animal < 3) {
-    		System.out.println("Animal Jovem");
-    		return;
-    	} 
-    	
-    	if (idade_animal < 10) {
-    		System.out.println("Animal Adulto");
-    		return;
-    	} 
-    	
-    	if (idade_animal >= 10) {
-    		System.out.println("Animal Idoso");
-    		return;
-    	}
-    }
 
-    public void emitirSom () {
-            System.out.println("Au au!");
+    public void envelhecer (int anos) {
+        if (anos < 0) {
+            System.out.println("Impossível :/");
             return;
         }
-    
+        if (anos == 0) {
+            System.out.println("Não envelheceu :D");
+            return;
+        }
+
+        if (this.idade + anos > 30) {
+            System.out.println("Idade máxima atingida :(");
+            return;
+        }
+
+        this.idade += anos;
+        String mensagem = String.format("Seu animal agora tem %d anos! ", idade); 
+        System.out.println(mensagem);
+        return;
+    }
+
+    public void trocarNome (String novo_nome) {
+        if (novo_nome == null) {
+            System.out.println("Nenhum nome foi digitado :(");
+            return;
+        }
+
+        if (novo_nome.length() == 1) {
+            System.out.println("Digite um nome com mais letras!");
+            return;
+        }
+
+        this.nome = novo_nome;
+        return;
+
+    }
+ 
 }
